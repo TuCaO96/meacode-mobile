9dadd2063d67cb8586609cd83f9c2f9b5cfbe4a6
arthurmr13@hotmail.com <amr962013>
2018-08-10 20:30:33 -0300

    First commit

---
+++ app/src/main/java/br/com/meacodeapp/meacodemobile/app/MeAcodeMobileApplication.java
@@ -0,0 +1,10 @@
+package br.com.meacodeapp.meacodemobile.app;
+
+import android.app.Application;
+
+/**
+ * Created by usuario on 10/08/2018.
+ */
+
+public class MeAcodeMobileApplication extends Application {
+}
