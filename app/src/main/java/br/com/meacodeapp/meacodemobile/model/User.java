f6d2fc332415db4a95edb163a19edb4d298e9769
Arthur Ribeiro <arthurmr13@hotmail.com>
2018-09-15 12:05:43 -0300

    Remove fields that will not be coming from the API

--- app/src/main/java/br/com/meacodeapp/meacodemobile/model/User.java
+++ app/src/main/java/br/com/meacodeapp/meacodemobile/model/User.java
@@ -8,12 +8,8 @@ public class User {
     private int id;
     private String username;
     private String email;
-    private String password;
-    private String password_hash;
     private String type;
     private String status;
-    private String password_reset_token;
-    private String auth_key;
     private String created_at;
     private String updated_at;
 }
