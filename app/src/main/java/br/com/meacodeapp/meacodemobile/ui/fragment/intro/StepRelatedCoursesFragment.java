7ac0838930a4ae7a2c7872fe01e60b8dc6b42dee
Arthur Ribeiro <arthurmr13@hotmail.com>
2018-12-02 22:55:18 -0200

    Add missing step

---
+++ app/src/main/java/br/com/meacodeapp/meacodemobile/ui/fragment/intro/StepRelatedCoursesFragment.java
@@ -0,0 +1,36 @@
+package br.com.meacodeapp.meacodemobile.ui.fragment.intro;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.meacodeapp.meacodemobile.R;
+import butterknife.ButterKnife;
+
+public class StepRelatedCoursesFragment extends Fragment {
+
+    public StepRelatedCoursesFragment() {
+        // Required empty public constructor
+    }
+
+    public static StepRelatedCoursesFragment newInstance() {
+        return new StepRelatedCoursesFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_step_five, container, false);
+        ButterKnife.bind(this, view);
+        // Inflate the layout for this fragment
+        return view;
+    }
+}
